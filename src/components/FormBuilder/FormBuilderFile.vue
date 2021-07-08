057786117aec653cf49e2630642c9a261d47fea1
kerasus <aliesmaeeli20@gmail.com>
2021-07-08 16:05:02 +0430

    update file input

--- src/components/FormBuilder/FormBuilderFile.vue
+++ src/components/FormBuilder/FormBuilderFile.vue
@@ -1,10 +1,39 @@
 <template>
-  <q-file @change="change($event)" v-model="file" :label="label" :disable="disable" :for="uid" clearable @clear="onClearInputFile" />
+  <q-file
+    v-model="file"
+    @update:model-value="change($event)"
+    @clear="onClearInputFile"
+    :label="label"
+    :disable="disable"
+    :for="uid"
+    clearable
+   :model-value="file"
+  />
   <label :for="uid">
     <q-img
-      v-if="canLoadImage"
+      v-if="previewMode === 'photo'"
       :src="imageSource"
       placeholder-src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJYAAACWBAMAAADOL2zRAAAAG1BMVEXMzMyWlpaqqqq3t7fFxcW+vr6xsbGjo6OcnJyLKnDGAAAACXBIWXMAAA7EAAAOxAGVKw4bAAABAElEQVRoge3SMW+DMBiE4YsxJqMJtHOTITPeOsLQnaodGImEUMZEkZhRUqn92f0MaTubtfeMh/QGHANEREREREREREREtIJJ0xbH299kp8l8FaGtLdTQ19HjofxZlJ0m1+eBKZcikd9PWtXC5DoDotRO04B9YOvFIXmXLy2jEbiqE6Df7DTleA5socLqvEFVxtJyrpZFWz/pHM2CVte0lS8g2eDe6prOyqPglhzROL+Xye4tmT4WvRcQ2/m81p+/rdguOi8Hc5L/8Qk4vhZzy08DduGt9eVQyP2qoTM1zi0/uf4hvBWf5c77e69Gf798y08L7j0RERERERERERH9P99ZpSVRivB/rgAAAABJRU5ErkJggg=="
+    />
+    <q-icon
+      v-if="previewMode === 'audio'"
+      size="2rem"
+      name="audiotrack"
+    />
+    <q-icon
+      v-if="previewMode === 'pdf'"
+      size="100px"
+      name="picture_as_pdf"
+    />
+    <q-icon
+      v-if="previewMode === 'office/word'"
+      size="100px"
+      name="description"
+    />
+    <q-icon
+      v-if="previewMode === 'office/excel'"
+      size="100px"
+      name="table_view"
     />
   </label>
 </template>
@@ -12,6 +41,7 @@
 <script>
 import { uid } from 'quasar'
 import inputMixin from 'components/FormBuilder/inputMixin'
+
 export default {
   name: 'FormBuilderFile',
   props: {
@@ -39,57 +69,83 @@ export default {
       }
     },
     inputData (newValue) {
-      if (this.isPhotoUrl(newValue)) {
+      if (this.isValidURL(newValue) && this.getSourceExtension(newValue) === 'photo') {
         this.url = newValue
       }
     },
     file (newValue) {
-      this.inputData = newValue
+      if (newValue === null) {
+        this.inputData = this.url
+      } else {
+        this.inputData = newValue
+      }
     }
   },
   computed: {
-    canLoadImage () {
-      if (this.isValidURL(this.inputData)) {
-        return this.isPhotoUrl(this.inputData)
-      } else if (this.inputData instanceof File) {
-        return this.isPhotoFile(this.inputData)
-      } else {
-        return false
-      }
-    },
     imageSource () {
       if (this.isValidURL(this.inputData)) {
         return this.inputData
       } else if (this.inputData instanceof File) {
         return this.createObjectURL()
+      } else if (this.inputData === null) {
+        return this.url
       } else {
         return null
       }
+    },
+    previewMode () {
+      let extension = ''
+      if (this.inputData === null) {
+        extension = this.getSourceExtension(this.url)
+      } else {
+        extension = this.getSourceExtension(this.inputData)
+      }
+
+      return extension
     }
   },
   methods: {
     onClearInputFile () {
-      if (this.inputData === null) {
-        this.inputData = this.url
-      }
+      // this.inputData = this.url
     },
-    isPhotoFile (file) {
-      if (!this.isFile(file) || typeof file.type !== 'string') {
+    getSourceExtension (filePath) {
+      if (this.isValidURL(filePath)) {
+        return this.getFileExtension(filePath)
+      } else if (this.isFile(filePath)) {
+        return this.getFileExtension(filePath.name)
+      } else {
         return false
       }
-      return file.type.match(/\/(jpeg|jpg|gif|png)$/) != null
+    },
+    getFileExtension (filePath) {
+      if (!filePath || typeof filePath !== 'string') {
+        return false
+      }
+      const photoExtensions = /\.(jpeg|jpg|gif|png)$/,
+        audioExtensions = /\.(mp3|ogg)$/,
+        officeWordExtensions = /\.(doc|docx)$/,
+        officeExcelExtensions = /\.(xls|xlsx)$/,
+        pdfExtensions = /\.(pdf)$/
+
+      if (filePath.match(photoExtensions) != null) {
+        return 'photo'
+      } else if (filePath.match(audioExtensions) != null) {
+        return 'audio'
+      } else if (filePath.match(pdfExtensions) != null) {
+        return 'pdf'
+      } else if (filePath.match(officeWordExtensions) != null) {
+        return 'office/word'
+      } else if (filePath.match(officeExcelExtensions) != null) {
+        return 'office/excel'
+      } else {
+        return false
+      }
     },
     isFile (file) {
-      return file instanceof File
-    },
-    isPhotoUrl (url) {
-      if (!url || typeof url !== 'string') {
-        return false
-      }
-      return (url.match(/\.(jpeg|jpg|gif|png)$/) != null)
+      return file instanceof File && typeof file.type === 'string'
     },
     isValidURL (str) {
-      if (!str || this.isFile(str)) {
+      if (!str || typeof str !== 'string' || this.isFile(str)) {
         return false
       }
       const a = document.createElement('a')
