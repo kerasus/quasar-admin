a08377d3b57e49fcd88b9e34bfdade878c92185e
mary_razmjoo <maryamrazmjoo76@yahoo.com>
2022-01-03 11:23:54 +0330

    coupon index

--- src/pages/Admin/Coupon/Index.vue
+++ src/pages/Admin/Coupon/Index.vue
@@ -1,22 +1,42 @@
 <template>
-<entity-index
-  v-model:value="inputs"
-  title="لیست بن ها"
-  :api="api"
-  :table="table"
-  :table-keys="tableKeys"
-  :create-route-name="createRouteName">
-</entity-index>
+  <portlet>
+    <template #title>
+      جدول فیلتر
+    </template>
+    <template #toolbar>
+      <q-btn flat round icon="add">
+        <q-tooltip>
+          اضافه کردن
+        </q-tooltip>
+      </q-btn>
+    </template>
+    <template #content>
+      <q-card class="category-card">
+        <form-builder v-model:value="filterInputs"/>
+      </q-card>
+    </template>
+  </portlet>
+  <entity-index
+    v-model:value="inputs"
+    title="لیست بن ها"
+    :api="api"
+    :table="table"
+    :table-keys="tableKeys"
+    :create-route-name="createRouteName">
+  </entity-index>
 </template>
 
 <script>
-import { EntityIndex } from 'quasar-crud'
+import { EntityIndex, Portlet } from 'quasar-crud'
 
 export default {
   name: 'Index',
-  components: { EntityIndex },
+  components: { EntityIndex, Portlet },
   data () {
     return {
+      filterInputs: [
+        { type: 'Input' }
+      ],
       inputs: [],
       api: 'v2/admin/coupon',
       table: [],
