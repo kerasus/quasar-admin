64bc4cb3684e228d27187cf4383e5a601c6afad5
mitraKhm <mitra.kh1995@gmail.com>
2022-01-03 11:22:09 +0330

    axios - token

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
     // colors.setBrand('primary', '#30A180')
     AddressbarColor.set('primary')
     if (this.$store.getters['Auth/user']) {
-      axios.defaults.headers.common.Authorization = this.$store.getters['Auth/tokenType'] + ' ' + this.$store.getters['Auth/token']
+      axios.defaults.headers.common.Authorization = this.$store.getters['Auth/tokenType'] + ' ' + this.$store.getters['Auth/accessToken']
     }
   }
 }
